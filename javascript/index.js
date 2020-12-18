e1b5a42345dc7acd0159467c16d8db131def4dc4
Or Bar <zokene@gmail.com>
2020-12-18 23:35:39 +0200

    first commit

---
+++ javascript/index.js
@@ -0,0 +1,15 @@
+var names = ["Or","Eden","Yali","Noga","Rom"];
+
+function whosPaying(names)
+{
+    var thePayMate = Math.floor(Math.random() * names.length) ;
+    console.log("the one whos paying is: ", names[thePayMate]);
+      
+     
+        
+
+    
+}     
+
+whosPaying(names);
+
